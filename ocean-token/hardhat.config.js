2772f4046501eb911633084db2a49531cc7f522b
chengcxi <143553765+chengcxi@users.noreply.github.com>
2023-09-16 18:10:49 -0700

    Update hardhat.config.js

--- ocean-token/hardhat.config.js
+++ ocean-token/hardhat.config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+  solidity: "0.8.21",
   networks: {
     rinkeby: {
       url: process.env.INFURA_RINKEBY_ENDPOINT,
