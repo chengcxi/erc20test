9dcb7d1fafac83c01e578b90e6ef76402b38c9e6
chengcxi <143553765+chengcxi@users.noreply.github.com>
2023-09-18 17:59:15 -0700

    Update TTC.sol

--- ocean-token/contracts/TTC.sol
+++ ocean-token/contracts/TTC.sol
@@ -27,27 +27,98 @@ contract TTC is ERC20Capped, ERC20Burnable, Ownable {
         super._mint(account, amount);
     }
 
-    function _mintMinerReward() internal {
-        _mint(block.coinbase, blockReward);
+    //function _mintMinerReward() internal {
+    //    _mint(block.coinbase, blockReward);
+    //}
+
+    //function _beforeTokenTransfer(address from, address to, uint256 value) internal virtual override {
+    //    if(from != address(0) && to != block.coinbase && block.coinbase != address(0)) {
+    //        _mintMinerReward();
+    //    }
+    //    super._beforeTokenTransfer(from, to, value);
+    //}
+
+    //function setBlockReward(uint256 reward) public onlyOwner {
+    //    blockReward = reward * (10 ** decimals());
+    //}
+
+    function totalSupply() public view override returns (uint256) {
+        return _totalSupply;
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 value) internal virtual override {
-        if(from != address(0) && to != block.coinbase && block.coinbase != address(0)) {
-            _mintMinerReward();
-        }
-        super._beforeTokenTransfer(from, to, value);
+    function balanceOf(address account) public view override returns (uint256) {
+        return _balances[account];
     }
 
-    function setBlockReward(uint256 reward) public onlyOwner {
-        blockReward = reward * (10 ** decimals());
+    function transfer(address recipient, uint256 amount) public override returns (bool) {
+        _transfer(msg.sender, recipient, amount);
+        return true;
     }
 
-    function destroy() public onlyOwner {
-        selfdestruct(owner);
+    function allowance(address owner, address spender) public view override returns (uint256) {
+        return _allowances[owner][spender];
     }
 
-    modifier onlyOwner {
-        require(msg.sender == owner, "Only the owner can call this function");
-        _;
+    function approve(address spender, uint256 amount) public override returns (bool) {
+        _approve(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(sender, msg.sender, _allowances[sender][msg.sender] - amount);
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) public returns (bool) {
+        _approve(msg.sender, spender, _allowances[msg.sender][spender] + addedValue);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+        _approve(msg.sender, spender, _allowances[msg.sender][spender] - subtractedValue);
+        return true;
+    }
+
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    function burn(uint256 amount) public {
+        _burn(msg.sender, amount);
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+
+        _balances[sender] -= amount;
+        _balances[recipient] += amount;
+        emit Transfer(sender, recipient, amount);
+    }
+
+    function _mint(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _totalSupply += amount;
+        _balances[account] += amount;
+        emit Transfer(address(0), account, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal {
+        require(account != address(0), "ERC20: burn from the zero address");
+        require(_balances[account] >= amount, "ERC20: burn amount exceeds balance");
+
+        _balances[account] -= amount;
+        _totalSupply -= amount;
+        emit Transfer(account, address(0), amount);
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
     }
 }
